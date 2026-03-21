958816fdeeeb39138b60e98317e682c3cc34d92c
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2026-03-21 11:02:37 +0900

    fea) [3/21] location 구현

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,19 +1,15 @@
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+import {Map, MapMarker} from "react-kakao-maps-sdk";
+import {LOCATION_INFO} from "../../constants/contact";
 
 const WeddingMap = () => {
     return (
         <Map
-            center={{ lat: 37.5665, lng: 126.9780 }} // 지도의 중심좌표
-            style={{ width: "100%", height: "450px" }} // 지도 크기
+            center={{lat: LOCATION_INFO.lat, lng: LOCATION_INFO.lng}} // 지도의 중심좌표
+            style={{width: "80%", height: "450px"}} // 지도 크기
             level={3} // 확대 레벨
         >
-            <MapMarker // 마커
-                position={{ lat: 37.5665, lng: 126.9780 }}
-                image={{
-                    src: "/marker.svg", // 준비하신 SVG 경로
-                    size: { width: 40, height: 40 },
-                }}
-            />
+            <MapMarker position={{lat: LOCATION_INFO.lat, lng: LOCATION_INFO.lng}}>
+            </MapMarker>
         </Map>
     );
 };
