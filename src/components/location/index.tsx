8c02076a3f7e2739224510a2c2f6f10933b56bb9
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2026-04-02 08:59:36 +0900

    fea) 지도/전화하기 구현

--- src/components/location/index.tsx
+++ src/components/location/index.tsx
@@ -3,30 +3,47 @@ import Typography from "@mui/material/Typography";
 import WeddingMap from "../map/index";
 import {LOCATION, LOCATION_CONTACT} from "../../constants/location";
 import locationImg from '@/assets/image/location.png';
-
+import Button from "@mui/material/Button";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Stack from '@mui/material/Stack'; // 버튼 간격을 위해 추천
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 
 const Location = () => {
     return (
-        <>
-            <div style={{backgroundColor: '#f0f0f0'}}>
-                <div className={styles.container}>
-                    <img src={locationImg} alt="location"/>
-                    <p className={styles.location}>
-                        {LOCATION}
-                    </p>
-                    <p className={styles.locationDetail}>
-                        {LOCATION_CONTACT.address}
-                    </p>
-                    <p className={styles.locationDetail}>
-                        {LOCATION_CONTACT.phoneNumber}
-                    </p>
-                    <WeddingMap/>
-                </div>
+        <div style={{backgroundColor: '#f0f0f0'}}>
+            <div className={styles.container}>
+                <img src={locationImg} alt="location"/>
+                <p className={styles.location}>
+                    {LOCATION}
+                </p>
+                <p className={styles.locationDetail}>
+                    {LOCATION_CONTACT.address}
+                </p>
+                <p className={styles.locationDetail}>
+                    {LOCATION_CONTACT.phoneNumber}
+                </p>
 
+                <Stack direction="row" spacing={5} justifyContent="center" sx={{my: 2}}>
+                    <Button
+                        variant="outlined"
+                        startIcon={<LocationOnIcon/>}
+                        onClick={() => window.open(`https://map.kakao.com/?q=${LOCATION_CONTACT.address}`)}
+                    >
+                        지도
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        startIcon={<LocalPhoneIcon/>}
+                        onClick={() => window.location.href = `tel:${LOCATION_CONTACT.phoneNumber}`}
+                    >
+                        전화하기
+                    </Button>
+                </Stack>
+
+                <WeddingMap/>
             </div>
-        </>
+        </div>
     )
 }
-
 
 export default Location;
