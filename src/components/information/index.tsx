1335b96db91ca4e7b99b8958752bc7b41255dedb
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2026-07-17 11:13:31 +0900

    feat: Invitaion 글자체 및 카드형태로 수정

--- src/components/information/index.tsx
+++ src/components/information/index.tsx
@@ -1,24 +1,27 @@
 import styles from './index.module.css';
-import letterDecor from '@/assets/image/our_wedding.png';
 
 const Information = () => {
     const greetingLines = [
-        "서로가 마주보며 다져온 사랑을",
-        "이제 함께 한 곳을 바라보며",
-        "걸어갈 수 있는 큰 사랑으로 키우고자 합니다.",
-        "저희 두 사람이 사랑의 이름으로",
-        "지켜나갈 수 있게 앞날을",
-        "축복해 주시면 감사하겠습니다."
+        "스쳐 지나가는 작은 웃음소리 하나마저도",
+        "서로에게 커다란 의미가 되는 단짝을 만났습니다.",
+        "낙엽이 지고 계절이 여물어가는 11월의 어느 멋진 날,",
+        "더 이상 옆구리 시리지 않게 평생 꼭 붙어 살기로 했습니다.",
+        "따뜻한 코트 한 벌 걸치고 오셔서",
+        "저희의 유쾌하고 훈훈한 계약을 축하해 주세요!"
     ];
 
     return (
         <div className={styles.container}>
-            <img src={letterDecor} alt="letter decor" className={styles.letterDecor}/>
-            {greetingLines.map((line, index) => (
-                <p key={index} className={styles.letter}>
-                    {line}
-                </p>
-            ))}
+            {/* 9:16 프레임과 동일한 스타일의 카드 프레임으로 감싸줍니다 */}
+            <div className={styles.cardFrame}>
+                <h2 className={styles.title}>Invitation</h2>
+
+                {greetingLines.map((line, index) => (
+                    <p key={index} className={styles.letter}>
+                        {line}
+                    </p>
+                ))}
+            </div>
         </div>
     );
 };
